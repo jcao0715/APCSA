02d528c9b9a2c0a8dd3012c4329ce3841f7a34b5
Jing Cao <jcao0715@gmail.com>
2018-12-25 13:08:19 -0500

    added test files to Monte Carlo

--- Module 05/Mod05 Assignments/05.07 Password Generator/PasscodeGenerator.java
+++ Module 05/Mod05 Assignments/05.07 Password Generator/PasscodeGenerator.java
@@ -6,10 +6,41 @@
  * @version 12/20/2018
  */
 
+import java.util.*;
+
  public class PasscodeGenerator
  {
  		public static void main(String [] args)
  		{
- 			
+ 			Scanner in = new Scanner(System.in);
+ 			int selection;
+ 			int length;
+
+ 			System.out.println("\t\t\tPassword Generator");
+ 			System.out.println("*************************************************************");
+ 			System.out.println("*	[1] Lowercase letters                               *");
+ 			System.out.println("*	[2] Lowercase and uppercase letters	            *");
+ 			System.out.println("*	[3] Lowercase letters and numbers                   *");
+ 			System.out.println("*	[4] Lowercase, uppercase, and numbers               *");
+ 			System.out.println("*	[5] Quit                                            *");
+ 			System.out.println("*************************************************************");
+
+ 			System.out.print("Enter selection (1-5): ");
+ 			selection = in.nextInt();
+ 			while(selection < 1 || selection > 5)
+ 			{
+ 				System.out.println("Invalid option. Please try again.");
+ 				System.out.println("Enter selection (1-5): ");
+ 				selection = in.nextInt();
+ 			}
+
+ 			System.out.println("Password length (more than 6 characters): ");
+ 			length = in.nextInt();
+ 			while(length <= 6)
+ 			{
+ 				System.out.println("Password length too short. Pease try again.");
+ 				System.out.println("Password length (more than 6 characters: ");
+ 				length = in.nextInt();
+ 			}
  		}
  }
