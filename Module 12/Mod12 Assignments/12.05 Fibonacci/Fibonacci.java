7c2c6e6d0a743574e0aba2b45ede72b49e994db1
Jing Cao <jcao0715@gmail.com>
2019-04-24 23:20:47 -0400

    added to Mod12 and made Mod13

---
+++ Module 12/Mod12 Assignments/12.05 Fibonacci/Fibonacci.java
@@ -0,0 +1,51 @@
+/**
+ * Purpose: Use a recursive method to print the Fibonacci sequence.
+ *
+ * @author Jing Cao
+ * @version 04/21/19
+ */
+
+import java.util.Scanner;
+
+public class Fibonacci
+{
+
+	public int fibonacci(int n) 
+	{
+		if(n == 0)
+		{
+			return 0;
+		}
+
+		else if(n == 1)
+		{
+			return 1;
+		}
+
+		else
+		{
+			return fibonacci(n - 1) + fibonacci(n - 2);
+		}
+    
+ 	}
+
+	public static void main(String [] args)
+	{
+		int x;
+		Fibonacci fib = new Fibonacci();
+		Scanner in = new Scanner(System.in);
+
+		System.out.println("Enter a non negative integer or -1 to exit the program.");
+		x = in.nextInt();
+
+		while(x != -1){
+
+		System.out.println(fib.fibonacci(x));
+		System.out.println("Enter a non negative integer or -1 to exit the program.");
+		x = in.nextInt();
+
+		}
+
+		System.out.println("You have exited the program.");
+	}
+}
