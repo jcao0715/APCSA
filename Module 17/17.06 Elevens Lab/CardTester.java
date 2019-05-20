f33a64eb949cac93c43fcf1ff84ba278127cbfbb
Jing Cao <jcao0715@gmail.com>
2019-05-19 23:29:00 -0400

    made folders Mod17 and Mod18

---
+++ Module 17/17.06 Elevens Lab/CardTester.java
@@ -0,0 +1,36 @@
+/**
+ * This is a class that tests the Card class.
+ *
+ *  < Your Name >
+ *  < Today's Date >
+ */
+
+public class CardTester
+{
+   /**
+    * The main method in this class checks the Card operations for consistency.
+    * @param args is not used.
+    */
+   public static void main(String[] args) {
+
+      /* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+
+      // Establish new cards
+      Card aceHearts = new Card("ace", "hearts", 1);
+
+      // Test card 1 and methods
+      System.out.println("**** Tests Card 1: ace of hearts ****");
+      System.out.println("  rank: " + aceHearts.rank());
+      System.out.println("  suit: " + aceHearts.suit());
+      System.out.println("  pointValue: " + aceHearts.pointValue());
+      System.out.println("  toString: " + aceHearts.toString());
+      System.out.println();
+
+      // Test card 2
+
+      // Test card 3
+
+      // Test matches() method
+
+   }
+}
