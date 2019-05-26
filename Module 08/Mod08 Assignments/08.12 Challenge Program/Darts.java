210b315e00aa47e7c9ed73f155ba244ef7edbcfc
Jing Cao <jcao0715@gmail.com>
2019-05-25 21:39:22 -0400

    fixed up 07.05 assignment

---
+++ Module 08/Mod08 Assignments/08.12 Challenge Program/Darts.java
@@ -0,0 +1,20 @@
+/**
+ * Purpose: Write a program to calculate the value of pi by simulating throwing darts at a dart board.
+ *
+ * @author Jing Cao
+ * @version 05/25/19
+ */
+
+public class Darts
+{
+	private int myDartThrows;
+	private int myNumTrials;
+
+	public Darts(int dartThrows, int numTrials)
+	{
+		myDartThrows = dartThrows;
+		myNumTrials = numTrials;
+	}
+
+	
+}
---
+++ Module 08/Mod08 Assignments/08.12 Challenge Program/DartsTester.java
@@ -0,0 +1,14 @@
+/**
+ * Purpose: Write a program to calculate the value of pi by simulating throwing darts at a dart board.
+ *
+ * @author Jing Cao
+ * @version 05/25/19
+ */
+
+public class DartsTester
+{
+	public static void main(String [] args)
+	{
+		
+	}
+}
